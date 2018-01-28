2564e3a52c5111f942b11260ab3647b7714b0f1f
deflou <35891055+deflou@users.noreply.github.com>
2018-01-28 21:09:11 +0300

    Create ICanBeDescribed.php

---
+++ src/interfaces/ICanBeDescribed.php
@@ -0,0 +1,43 @@
+<?php
+namespace deflou\interfaces;
+
+/**
+ * ICanBeDescribed
+ */
+interface ICanBeDescribed
+{
+    /**
+     * @return string
+     */
+    public function getName(): string;
+    
+    /**
+     * @return string
+     */
+    public function getTitle(): string;
+    
+    /**
+     * @return string
+     */
+    public function getDescription(): string;
+    
+    /**
+     * @return $this
+     */
+    public function setName($name);
+    
+    /**
+     * @return $this
+     */
+    public function setTitle($title);
+    
+    /**
+     * @return $this
+     */
+    public function setDescription($description);
+    
+    /**
+     * @return mixed
+     */
+    public function describe();
+}

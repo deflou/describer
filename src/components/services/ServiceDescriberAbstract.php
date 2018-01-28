41226653744581e1ea2b9a721adb643d980d18ac
deflou <35891055+deflou@users.noreply.github.com>
2018-01-28 21:38:11 +0300

    Update ServiceDescriberAbstract.php

--- src/components/services/ServiceDescriberAbstract.php
+++ src/components/services/ServiceDescriberAbstract.php
@@ -61,4 +61,145 @@ abstract class ServiceDescriberAbstract implements IServiceDescriber
         
         return $this;
     }
+    
+    /**
+     * @return IServiceConfig
+     * @throws \Exception
+     */
+    public function getServiceConfig(): IServiceConfig
+    {
+        if (!$this->serviceConfig) {
+            throw new \Exception('Service config is not initialized or set.');
+        }
+        
+        return $this->serviceConfig;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getServiceName(): string
+    {
+        return $this->getServiceConfig()->getServiceName();
+    }
+    
+    /**
+     * @return string
+     */
+    public function getServiceTitle(): string
+    {
+        return $this->getServiceConfig()->getServiceTitle();
+    }
+    
+    /**
+     * @return string
+     */
+    public function getServiceDescription(): string
+    {
+        return $this->getServiceConfig()->getServiceDescription();
+    }
+    
+    /**
+     * @return string
+     */
+    public function getServiceVersion(): string
+    {
+        return $this->getServiceConfig()->getServiceVersion();
+    }
+    
+    /**
+     * @return string Resolver class name
+     */
+    public function getServiceResolver(): string
+    {
+        return $this->getServiceConfig()->getServiceResolver();
+    }
+    
+    /**
+     * @return IServiceAuthor[]
+     */
+    public function getServiceAuthors(): array
+    {
+        $authorsConfigs = $this->getServiceConfig()->getServiceAuthors();
+        $authors = [];
+        
+        foreach ($authorsConfigs as $authorConfig) {
+            $authors[] = new ServiceAuthorBase($authorConfig);
+        }
+        
+        return $authors;
+    }
+    
+    /**
+     * @return IServiceOptionConfigured[]
+     */
+    public function getServiceOptions(): array
+    {
+        $optionsConfigs = $this->getServiceConfig()->getServiceOptions();
+        $options = [];
+        
+        foreach ($optionsConfigs as $optionConfig) {
+            $options = new ServiceOptionConfigured($optionConfig);
+        }
+        
+        return $options;
+    }
+    
+    /**
+     * @param string $optionName
+     *
+     * @return IServiceOptionConfigured
+     * @throws \Exception
+     */
+    public function getOption($optionName): IServiceOptionConfigured
+    {
+        if ($this->getServiceConfig()->hasServiceOption($optionName)) {
+            return new ServiceOptionConfigured($this->getServiceConfig()->getServiceOption($optionName));
+        }
+        
+        throw new \Exception('Unknown or missed option "' . $optionName . '"');
+    }
+    
+    /**
+     * @return IServiceEvent[]
+     */
+    public function getServiceEvents(): array
+    {
+        $eventsConfigs = $this->getConfig()->getServiceEvents();
+        $events = [];
+        
+        foreach ($eventsConfigs as $eventConfig) {
+            $events[] = new ServiceEventBase($eventConfig);
+        }
+        
+        return $events;
+    }
+    
+    /**
+     * @return IServiceAction[]
+     */
+    public function getServiceActions(): array
+    {
+        $actionsConfigs = $this->getConfig()->getServiceActions();
+        $actions = [];
+        
+        foreach ($actionsConfigs as $actionConfig) {
+            $actions[] = new ServiceEventBase($actionConfig);
+        }
+        
+        return $actions;
+    }
+    
+    /**
+     * @param string $optionName
+     * @param string $optionValue
+     *
+     * @return IServiceOptionDescribed
+     */
+    public function describeOption($optionName, $optionValue): IServiceOptionDescribed
+    {
+        return method_exists($this, $optionName . 'Describe')
+            ? $this->{$optionName . 'Describe'}($optionValue)
+            : new ServiceOptionDescribed($this->getOption($optionName), $optionValue);
+    }
 }
